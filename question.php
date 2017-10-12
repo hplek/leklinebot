a911742528c9e7242b48e95460b698eb406b8b23
hplek <32577718+hplek@users.noreply.github.com>
2017-10-12 12:08:50 +0700

    Create question.php

---
+++ question.php
@@ -0,0 +1,16 @@
+<?php
+    $question = [
+        0 => [
+            'keywords' => 'สี',
+            'ans' => 'สีเหลือง'
+        ],
+        1 => [
+            'keywords' => 'สี',
+            'ans' => 'สีเหลือง'
+        ]
+    ];
+
+    foreach($question as $row){
+         echo $row['ans'];
+    }
+?>
