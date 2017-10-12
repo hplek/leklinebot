e176bb699d4f12875cff8fc55ea919936129699e
hplek <32577718+hplek@users.noreply.github.com>
2017-10-12 21:16:44 +0700

    Update testConnection.php

--- testConnection.php
+++ testConnection.php
@@ -11,7 +11,7 @@ $db = substr($url["path"], 1);
 
     $key = $_POST["key"];
     $ans = $_POST["ans"];
-    $sql = "INSERT INTO `heroku_da1dc32cdc85254`.`knowledge`(`key`,`ans`) VALUES ('$key','$ans')";
+    $sql = "INSERT INTO `heroku_cc65da134c5a8d1`.`knowledge`(`key`,`ans`) VALUES ('$key','$ans')";
     
     if($conn->query($sql)){
         echo 'success';
