1c0dbfd7fa03a6b19e910c25f7fc8078b866be6f
Saksit Imsorn <annochan@gmail.com>
2017-10-13 12:38:30 +0700

    gg

--- status.php
+++ status.php
@@ -6,4 +6,12 @@
     
     printf("Initial character set: %s\n", $conn->character_set_name());
 
+    /* change character set to utf8 */
+    if (!$conn->set_charset("utf8")) {
+        printf("Error loading character set utf8: %s\n", $conn->error);
+        exit();
+    } else {
+        printf("Current character set: %s\n", $conn->character_set_name());
+    }
+
 ?>
