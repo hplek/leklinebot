1df16e19cc8118a61ff7543ce61934830185e13e
hplek <32577718+hplek@users.noreply.github.com>
2017-10-14 00:08:51 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,13 +2,13 @@
 <html>
     <head>
         <meta http-equiv=Content-Type content="text/html; charset=utf-8">
-        <title>Bot Spoon Management</title>
+        <title>Spoon Management</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     </head>
 <body>
-<nav class="navbar navbar-toggleable-md navbar-inverse" style="background-color: yellow;">
-    <a class="navbar-brand pull-middle" href="#"><strong>Manage Bot Data</strong></a>
+<nav class="navbar navbar-toggleable-md navbar-inverse" style="background-color: orange;">
+    <a class="navbar-brand pull-middle" href="#"><strong>Manage Khun Spoon Data</strong></a>
 </nav>
 
 <?php 
